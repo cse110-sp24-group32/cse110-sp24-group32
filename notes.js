f492cc1a2a1135add4f5a37ff953e9aff33db5ae
chris-529 <chris-529@users.noreply.github.com>
2024-05-23 01:36:07 +0000

    deploy: 7f9b890460a06244f08f8d0aa63599d27c5e3182

--- notes.js
+++ notes.js
@@ -11,12 +11,5 @@ class Note {
     this.id = self.crypto.randomUUID()
     console.log(this.id)
   }
-
-  addTag(tag) {
-    if (!this.tags.includes(tag)) {
-      this.tags.push(tag);
-    }
-  }
-
 }
 export { Note }
