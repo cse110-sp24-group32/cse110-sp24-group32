3d7217d5b5d5d6403b1e8d177f3955beebbd1746
philipchi <cyuhsuan@sdsc.edu>
2024-05-24 21:29:35 -0700

    search event listener fix

--- source/index.js
+++ source/index.js
@@ -250,38 +250,6 @@ function init () {
     console.log(editor.value, man.curNoteId)
     man.getNote(man.curNoteId).content = editor.value
   })
-
-  // search bar functionality
-  // listens for keyup, runs search if it's the Enter key
-  searchBar.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      searchBar.blur()
-
-      // if search term is empty, don't display results
-      if (searchBar.value.length === 0) { return }
-
-      const resultsContainer = document.getElementById('search-results-container')
-      //document.getElementById('note-view').classList.add('hidden')
-
-      // clear results
-      resultsContainer.textContent = '';
-      resultsContainer.classList.remove('hidden');
-
-      // populate search results with placeholders
-      for (let i = 1; i < 5; i++) {
-        const sr = document.createElement('div')
-        sr.className = 'search-result'
-        const srTitle = document.createElement('span')
-        srTitle.className = 'result-title'
-        srTitle.innerText = 'Note Title ' + String(i)
-        const srPreview = document.createElement('span')
-        srPreview.className = 'result-preview'
-        srPreview.innerText = 'This is a preview of the note\'s contents'
-        sr.append(srTitle, document.createElement('br'), srPreview)
-        resultsContainer.append(sr)
-      }
-    }
-  })
   
   // Hide search results if you click somewhere else on the page
   document.querySelector('body').addEventListener('click', (event) => {
--- source/search.js
+++ source/search.js
@@ -15,6 +15,7 @@ function search(query) {
     }
 
     for (const tag in note.tags) {
+      console.log(tag)
       if (tag.includes(query)) {
         match_notes.push(note);
         break;
@@ -26,9 +27,44 @@ function search(query) {
 }
 
 const searchBar = document.getElementById('search-bar');
-
+/*
 searchBar.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
+    const match_notes = search(searchBar.value);
+    console.log(match_notes)
+  }
+})*/
+
+// search bar functionality
+// listens for keyup, runs search if it's the Enter key
+searchBar.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    searchBar.blur()
+
+    // if search term is empty, don't display results
+    if (searchBar.value.length === 0) { return }
+
+    const resultsContainer = document.getElementById('search-results-container')
+    //document.getElementById('note-view').classList.add('hidden')
+
+    // clear results
+    resultsContainer.textContent = '';
+    resultsContainer.classList.remove('hidden');
+
+    // populate search results with placeholders
+    for (let i = 1; i < 5; i++) {
+      const sr = document.createElement('div')
+      sr.className = 'search-result'
+      const srTitle = document.createElement('span')
+      srTitle.className = 'result-title'
+      srTitle.innerText = 'Note Title ' + String(i)
+      const srPreview = document.createElement('span')
+      srPreview.className = 'result-preview'
+      srPreview.innerText = 'This is a preview of the note\'s contents'
+      sr.append(srTitle, document.createElement('br'), srPreview)
+      resultsContainer.append(sr)
+    }
+
     const match_notes = search(searchBar.value);
     console.log(match_notes)
   }
