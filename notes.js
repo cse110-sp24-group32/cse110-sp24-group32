f1c65b381c9104b87e7d6abc634401a0f2919a57
chris-529 <chris-529@users.noreply.github.com>
2024-05-31 03:03:58 +0000

    deploy: af92031b6796dc8370b50933ac483aa9c233b001

--- notes.js
+++ notes.js
@@ -1,15 +1,21 @@
 /**
- * Data for note
- * Contains project id, content, title, tags list, and a UUID for the note
+ * Represents a note with project ID, content, title, tags, and a unique ID.
  */
 class Note {
-  constructor (p, c, t, l) {
-    this.proj = p
-    this.content = c
-    this.title = t
-    this.tags = l
-    this.id = self.crypto.randomUUID()
-    console.log(this.id)
+  /**
+   * Create a note.
+   * @param {string} p - The project ID.
+   * @param {string} c - The content of the note.
+   * @param {string} t - The title of the note.
+   * @param {Array} l - The list of tags.
+   */
+  constructor(p, c, t, l) {
+    this.proj = p;
+    this.content = c;
+    this.title = t;
+    this.tags = l;
+    this.id = self.crypto.randomUUID();
+    console.log(this.id);
   }
 }
-export { Note }
+export { Note };
