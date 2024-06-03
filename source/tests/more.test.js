fe08af6b3c4ffd9207228a3cda3ba56652e4a975
Thomas Li <zhl135@ucsd.edu>
2024-06-02 19:16:20 -0700

    Add search E2E tests, remove debug logs from Manager

---
+++ source/tests/more.test.js
@@ -0,0 +1,88 @@
+import puppeteer from 'puppeteer'
+
+// Puppeteer tests all go in here
+describe('Additional E2E tests', () => {
+  let browser
+  let page
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false, // Set to false if you want to see the browser during the tests
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      slowMo: 25
+    })
+    page = await browser.newPage()
+    await page.goto('http://localhost:3000/index.html') // Use local server URL
+  })
+
+  afterAll(async () => {
+    await browser.close()
+  })
+
+  // Test search functionality
+  // We need to do some setup to search something
+  it('shouldnt be possible to add a note without a project selected', async () => {
+    let btn = await page.$('#new-project-button')
+    await btn.click()
+    const pName = await page.$('#project-input')
+    await pName.type('project')
+    btn = await page.$('#project-input')
+    btn = await page.$('#confirm-new-project')
+    await btn.click()
+
+    btn = await page.$('.project-icon')
+    await btn.click()
+
+    btn = await page.$('#add-note')
+    await btn.click()
+
+    btn = await page.$('#Meeting')
+    await btn.click()
+
+    await page.$('#note-input').then((i) => { return i.type('note') }).then((i) => {
+      return page.$('#choose-note')
+    }).then((i) => { return i.click() })
+
+    btn = await page.$('#add-note')
+    await btn.click()
+
+    btn = await page.$('#Code')
+    await btn.click()
+
+    await page.$('#note-input').then((i) => { i.type('1') }).then((i) => {
+      return page.$('#choose-note')
+    }).then((i) => { return i.click() })
+
+    // find meeting note
+    const search = await page.$('#search-bar')
+    await search.type('meet')
+    await page.keyboard.press('Enter')
+    let res = await page.$$('.search-result')
+    expect(res.length).toBe(1)
+
+    // find both notes
+    await search.click({ clickCount: 3 })
+    await search.press('Backspace')
+    await search.type('c')
+    await page.keyboard.press('Enter')
+    res = await page.$$('.search-result')
+    expect(res.length).toBe(2)
+
+    // find no notes
+    await search.click({ clickCount: 3 })
+    await search.press('Backspace')
+    await search.type('BITSET')
+    await page.keyboard.press('Enter')
+    res = await page.$$('.search-result')
+    expect(res.length).toBe(1)
+    let txt = await page.$eval('.search-result', el => el.innerText)
+    expect(txt).toBe('no matching result')
+
+    // test empty field, should not show results
+    await search.click({ clickCount: 3 })
+    await search.press('Backspace')
+    await page.keyboard.press('Enter')
+    txt = await page.$eval('#search-results-container', el => el.className)
+    expect(txt).toBe('hidden')
+  }, 1e9)
+})
